510563451a2c1d81c1003c60823e8fe18197dfef
MumenAlmadaineh <mumen_almadaineh@outlook.com>
2021-09-12 22:06:06 +0300

    Get data from the form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import CityLocation from './component/CityLocation'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import RecyclerCity from './component/RecyclerCity';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      cityName: '',
+      lon: '',
+      lat: '',
+      flag: false,
+    };
+  };
+  getCityName = e => {e.preventDefault();
+    let cityObj = {
+      method: 'GET',
+      baseURL:`https://api.locationiq.com/v1/autocomplete.php?key=${process.env.REACT_APP_API_KEY_FOR_CITY_PROJECT}&q=${this.state.cityName}`,
+    }
+    axios(cityObj).then(re => 
+      {
+        let axArray = re.data[0];
+        this.setState(
+          {
+            cityName: axArray.display_name,
+            lon: axArray.lon,
+            lat: axArray.lat,
+            flag: true,
+          })
+      })
+
+  } 
+
+  getFromUser = e => {
+    let cityNameEvent = e.target.value;
+    this.setState({
+      cityName: cityNameEvent,
+    })
+  }
+  render() {
+    return (
+      <div>
+        <CityLocation getCityName={this.getCityName} getFromUser={this.getFromUser} />
+        {
+          this.state.flag && <RecyclerCity cityName = {this.state.cityName} lon = {this.state.lon} lat = {this.state.lat} />
+        }
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
