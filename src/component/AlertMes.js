95a3a1dc5f8d94a32503fc4bc3cac9c4d24d1e12
MumenAlmadaineh <mumen_almadaineh@outlook.com>
2021-09-14 22:37:09 +0300

    lab8

---
+++ src/component/AlertMes.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Alert } from 'react-bootstrap'
+
+export class AlertMes extends Component {
+    render() {
+        return (
+                <Alert variant='denger'>
+                    Threre is an error
+                </Alert>
+        )
+    }
+}
+
+export default AlertMes 
